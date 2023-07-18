f90fedd78c84d51e68931c8b7ae29b2e17160ac8
confett0 <118047961+confett0@users.noreply.github.com>
2023-07-18 21:09:12 +0200

    Display today's other info

--- src/ui.js
+++ src/ui.js
@@ -3,10 +3,19 @@ function showWeather(weather) {
     const todayTemp = document.querySelector(".today-temperature");
     const location = document.querySelector(".location");
     const todayCondition = document.querySelector(".today-condition");
+    const perceivedTemperature = document.querySelector(".feels-like");
+    const humidity = document.querySelector(".humidity");
+    const rain = document.querySelector(".rain");
+    const wind = document.querySelector(".wind");
 
     todayTemp.textContent = `${weather.current.temp_c} ºC`;
     location.textContent = weather.location.name;
     todayCondition.textContent = weather.current.condition.text;
+    perceivedTemperature.textContent = `${weather.current.feelslike_c} ºC`;
+    humidity.textContent = `${weather.current.humidity}%;`;
+    rain.textContent = `${weather.forecast.forecastday[0].day.daily_chance_of_rain}%`;
+    wind.textContent = `${weather.current.wind_kph} km/h`;
+
 }
 
 export { showWeather };
